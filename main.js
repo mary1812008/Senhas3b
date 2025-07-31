bf89a87800bff7e469d3f6515e6651f65e57c92a
mary1812008 <164516580+mary1812008@users.noreply.github.com>
2025-07-31 12:34:58 +0000

    vcc

--- main.js
+++ main.js
@@ -22,18 +22,46 @@ function aumentaTamanho() {
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-
 const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox')
 
+for(i = 0; i < checkbox.length; i++){
+    checkbox[i].onlick = geraSenha;
+}
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ';
+const letrasMinuscular = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#$%*?';
 geraSenha();
 
 function geraSenha() {
+    let alfabeto = '';
+    if (checkbox [0] . checkbox) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox [1] . checkbox) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    function geraSenha() {
+    let alfabeto = '';
+    if (checkbox [2] . checkbox) {
+        alfabeto = alfabeto + numeros;
+    }
+    function geraSenha() {
+    let alfabeto = '';
+    if (checkbox [3] . checkbox) {
+        alfabeto = alfabeto + simbolos;
+    }
+
+ 
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
-        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alabeto[numeroAleatorio];
     }
+    {
     campoSenha.value = senha;
-}
+    }
